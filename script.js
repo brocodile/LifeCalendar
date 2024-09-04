4af8b4282fe671ec98d55ad48c0745b6723951c6
brocodile <dakzsh24@gmail.com>
2024-09-05 01:24:21 +0530

    Complete date extraction

--- script.js
+++ script.js
@@ -14,16 +14,25 @@ const submit = document.querySelector("#btn");
 
 submit.addEventListener("click", ()=>{
   let birth = date.value;
-  let newB = birth.slice(5,7) + "-" + birth.slice(8,11) + "-" + birth.slice(0,4);
+  let newB = birth.slice(5,7) + "/" + birth.slice(8,11) + "/" + birth.slice(0,4);
   const toDdate = new Date();
 
   let day = toDdate.getDate();
   let month = toDdate.getMonth() + 1;
   let year = toDdate.getFullYear();
 
-  let currentDate = `${month}-${day}-${year}`;
-  console.log(newB);
-  console.log(currentDate); // "17-6-2022"
+  let currentDate = `${month}/${day}/${year}`;
+
+  let totDays = dateCalc(newB,currentDate);
+  console.log(totDays);
 
   
 })
+
+function dateCalc(d1,d2){
+    let date1 = new Date(d1);  
+    let date2 = new Date(d2);  
+    let time_difference = date2.getTime() - date1.getTime();  
+    let days_difference = time_difference / (1000 * 60 * 60 * 24);
+    return days_difference;
+}
