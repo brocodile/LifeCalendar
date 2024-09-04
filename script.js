8367f56c2a31218ea7645fe9795059868c2a0dbb
brocodile <dakzsh24@gmail.com>
2024-09-05 00:06:58 +0530

    Add UI

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let container = document.querySelector(".container");
 for(let i = 1; i<4681; i++){
     const box = document.createElement("div");
     box.setAttribute("class", "box");
-    box.textContent = i;
+    // box.textContent = i;
     if(i != 0 && i % 5 == 0){
         box.style["marginRight"] = "5px";
     } 
