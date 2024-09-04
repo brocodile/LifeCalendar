e1170eb8a864e5d4f38ca7038e4680c286ab1901
brocodile <dakzsh24@gmail.com>
2024-09-05 01:16:26 +0530

    Add Calendar Input

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ for(let i = 1; i<4681; i++){
     } 
     container.appendChild(box);
 }
+
+const date = document.querySelector("#dateTime");
+const submit = document.querySelector("#btn");
+
+submit.addEventListener("click", ()=>{
+  let birth = date.value;
+  let newB = birth.slice(5,7) + "-" + birth.slice(8,11) + "-" + birth.slice(0,4);
+  const toDdate = new Date();
+
+  let day = toDdate.getDate();
+  let month = toDdate.getMonth() + 1;
+  let year = toDdate.getFullYear();
+
+  let currentDate = `${month}-${day}-${year}`;
+  console.log(newB);
+  console.log(currentDate); // "17-6-2022"
+
+  
+})
